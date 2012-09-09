797c4a623689f68c5afdc6372436de42ff5ffdbf
Ben Glassman <ben@vtdesignworks.com>
2012-09-09 14:10:12 -0400

    Add python rsync script to sync a file, directory or glob to a remote. rsync.send_to_project uses projectpath.find to automatically determined estination.

--- python/projectpath.py
+++ python/projectpath.py
@@ -6,7 +6,7 @@ def find(start_path, indicator_filename='.root-dir'):
 	
 	project_path = False
 	current_path = os.path.abspath(os.path.expanduser(start_path))
-	if (os.path.isfile(current_path)):
+	if (os.path.isdir(current_path) != 1):
 		current_path = os.path.dirname(current_path)
 
 	while True:
---
+++ python/rsync.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+import subprocess
+import projectpath
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('source')
+args = parser.parse_args()
+
+opts = "-rltD --progress --exclude='*.swp' --exclude='*.bak'"
+ssh_config_path = os.path.abspath(os.path.expanduser('~/.ssh/config'))
+
+def send(source, destination, ssh_alias, opts=opts):
+
+	cmd = 'rsync ' + opts + ' "' +  source + '" ' + ssh_alias + ':' + destination
+	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	return proc.communicate()
+
+def send_to_project(source):
+
+	source = os.path.abspath(os.path.expanduser(source))
+	project_path = projectpath.find(source)
+
+	if project_path == False:
+		return False
+
+	# if its a directory or if the path doesnt exist, send to parent directory on remote
+	remote_path = source.replace(project_path + os.path.sep, '')
+	if os.path.isdir(source) or os.path.exists(source) == False: 
+		remote_path = os.path.sep.join(remote_path.split(os.path.sep)[0:-1])
+
+	# TODO Allow for .root-dir to contain project data such as ssh alias
+	# Project Alias for SSH is last dir
+	project_alias = project_path.split(os.path.sep)[-1]
+
+	if is_ssh_alias(project_alias):
+		result = send(source, remote_path, project_alias)
+		return result[0]
+	else:
+		return False
+
+def is_ssh_alias(alias, path=ssh_config_path):
+
+	cmd = "grep -cE '^Host .*" + alias + "( |$)' " + path
+	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	stdout, stderr = proc.communicate()
+	return int(stdout.strip())
