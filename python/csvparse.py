b3b455e5beb202b438cee673a8e19fdc3ef5c425
Ben Glassman <ben@vtdesignworks.com>
2014-04-21 12:20:53 -0400

    Remove duplicate csvparse

--- python/csvparse.py
+++
@@ -1,7 +0,0 @@
-def showFields(fields):
-	import vim
-	import csv
-	reader = csv.reader(vim.current.buffer, delimiter=',')
-	for row in reader:
-		print [ value for field, value in enumerate(row) if field in fields ]
-
