63098d69b8667426f02a38e97645d70bd8c95346
Ben Glassman <ben@vtdesignworks.com>
2016-04-03 14:01:23 -0400

    Change symlink to install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.config/neovim
 ln -s ~/dotfiles/vim ~/.config/neovim
+ln -s ~/dotfiles/vimrc ~/.config/neovim/init.vim
 ln -s ~/dotfiles/vim ~/.vim 
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc 
 ln -s ~/dotfiles/vim/gvimrc ~/.gvimrc 
